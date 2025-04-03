c8cd230b9d7a8f9476e37010e002e0dfefd4bb85
seth shoemaker <contact@sethshoemaker.com>
2025-04-03 13:46:33 -0400

    enhance search, catalog generation script

--- catalog_scraper/__main__.py
+++ catalog_scraper/__main__.py
@@ -4,8 +4,9 @@ import jsonpickle
 from rich.panel import Panel
 from rich.columns import Columns
 from catalog_scraper import console
+from datetime import datetime
 
-async def main():
+async def fetch_courses_file():
     console.log(f"[bold yellow]Fetching Course Prefixes[/]")
     prefixes = await lib.get_course_prefix_list()
     console.log(f"[bold green]Fetch complete[/]")
@@ -23,6 +24,17 @@ async def main():
     output.write(jsonpickle.encode(courses, indent=4))
     output.close()
     console.log("[bold green]Upload Complete[/]")
+
+async def main():
+    # await fetch_courses_file()
+    courses_json = open('output.json', 'r')
+    courses_json_content = courses_json.read()
+    catalog_html = open('catalog.html', 'r')
+    catalog_html_content = catalog_html.read()
+    catalog_html_content = catalog_html_content.replace('const courses = []', f'const courses = {courses_json_content}')
+    catalog_html_content = catalog_html_content.replace('Updated on DATE', f'Updated on {datetime.now().strftime("%B %d, %Y at %I:%M %p")}')
+    catalog_built = open('catalog_output.html', 'w')
+    catalog_built.write(catalog_html_content)
 
 if __name__ == "__main__":
     asyncio.run(main())
